9820709f8ca2305e6087d036a2598aab4cd13ead
Ivyy7968 <faujifaujian@gmail.com>
2026-09-11 20:54:27 +0700

    Clean bootstrap cache and update api/index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
 $_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 
@@ -17,18 +13,8 @@ $dirs = [
 
 foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
+        @mkdir($dir, 0755, true);
     }
 }
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
