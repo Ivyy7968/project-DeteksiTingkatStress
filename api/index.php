2045b6ef41c702a0199ec1054f5faa1688902667
Ivyy7968 <faujifaujian@gmail.com>
2026-09-12 15:15:18 +0700

    Add Vercel deployment configuration

--- api/index.php
+++ api/index.php
@@ -1,20 +1,3 @@
 <?php
 
-$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
-
-$dirs = [
-    '/tmp/storage/app',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/framework/views',
-    '/tmp/storage/logs',
-];
-
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        @mkdir($dir, 0755, true);
-    }
-}
-
 require __DIR__ . '/../public/index.php';
